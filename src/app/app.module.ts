30f767d1abb2d3d511fb38ddf9853491d90f781d
Dzmitry Iliukovich <mr.ilyukovich@gmail.com>
2022-03-21 23:45:09 +0300

    feat: add base compoents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { ButtonComponent } from './button/button.component';
+import { LogoComponent } from './logo/logo.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    ButtonComponent,
+    LogoComponent,
+    CardComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
