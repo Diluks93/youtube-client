8aca23977f5369d3fbfb7b409bef44ec437857eb
Dzmitry Iliukovich <mr.ilyukovich@gmail.com>
2022-03-28 22:35:05 +0300

    feat: add click on setting's button

--- src/app/components/header/header/header.component.ts
+++ src/app/components/header/header/header.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Output() isShowFilterComponent = new EventEmitter();
+
+  public showFilterComponent(): void {
+    this.isShowFilterComponent.emit(true);
+  }
+}
